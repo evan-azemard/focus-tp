24e8bd4ca4d7f871574ac455c6a8041247c35663
evan <evan.azemard@acteam-it.com>
2025-06-05 14:38:53 +0200

    chore(app): add cookieParser middleware and configure routes

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -2,7 +2,8 @@
 import express, { Request, Response, NextFunction} from "express";
 import cors from "cors";
 import { env } from "./config/env";
-
+import router from "./routes/index"; // Importation du routeur principal
+import cookieParser from "cookie-parser";
 
 const app = express();
 const { PORT, ORIGIN } = env;
@@ -12,10 +13,11 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Méthodes HTTP autorisées
     credentials: true, // Autorise les cookies et les en-têtes d'authentification
 }))
+app.use(cookieParser()); // Pour parser les cookies dans les requêtes entrantes
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); // Pour parser les données URL-encodées
 
-app.use('/', router); // TODO création du router
+app.use('/', router);
 
 app.listen(PORT, () => {
     console.log('Le serveur est en écoute sur le port', PORT);
