e8d5901db40be62e32bf5109026c44375e59697c
evan <evan.azemard@acteam-it.com>
2025-06-06 10:35:41 +0200

    chore(index): add index files

---
+++ packages/server/src/controllers/index.ts
@@ -0,0 +1,6 @@
+export { default as authController } from "./auth.controller";
+export { default as goalController } from "./goal.controller";
+export { default as stepController } from "./step.controller";
+export { default as substepController } from "./substep.controller";
+export { default as taskController } from "./task.controller";
+export { default as userController } from "./user.controller";
--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -1,16 +1,19 @@
 // Définition des routes de l'application
-import {Router } from "express";
+import { Router } from "express";
 import goalRouter from './goals.routes';
 import stepRouter from './steps.routes';
 import substepRouter from './substeps.routes';
 import taskRouter from './tasks.routes';
 import userRouter from './users.routes';
+import authRouter from './auth.routes';
+import { isAuthentificated } from "../middlewares";
 const router = Router();
 
-router.use('/goals', goalRouter);
-router.use('/steps', stepRouter);
-router.use('/substeps', substepRouter);
-router.use('/tasks', taskRouter);
-router.use('/users', userRouter);
+router.use('/goals', isAuthentificated, goalRouter);
+router.use('/steps', isAuthentificated, stepRouter);
+router.use('/substeps', isAuthentificated, substepRouter);
+router.use('/tasks', isAuthentificated, taskRouter);
+router.use('/users', isAuthentificated, userRouter);
+router.use('/auth', authRouter);
 
 export default router;
