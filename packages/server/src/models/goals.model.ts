18281f55009522602f3d5f0c4748b0330f24a8fa
evan <evan.azemard@acteam-it.com>
2025-06-06 02:54:25 +0200

    refactor(goalModel): clean structure, improved error handling and logging

--- packages/server/src/models/goals.model.ts
+++ packages/server/src/models/goals.model.ts
@@ -1,12 +1,11 @@
-// ? Ce fichier contient le modèle pour les objectifs, il est utilisé pour interagir avec la base de données.
-
+// ! Ce fichier contient le modèle pour gérer les objectifs d'un utilisateur
 import { Goal, NewGoal } from '../entities/Goal.entity';
 import { db } from '../config/pool';
-import { goals, steps, substeps, tasks } from '../schemas';
-import logger from '../utils/logger';
+import { goals } from '../schemas';
+import logger, { logError } from '../utils/logger';
 import { and, eq } from 'drizzle-orm';
 
-// Relations imbriquées
+// ? Relations imbriquées pour plus de lisibilité des données
 const taskWith = {
     columns: { id: true, title: true, description: true, substepId: true, dueDate: true, status: true, createdAt: true, updatedAt: true }
 };
@@ -22,78 +21,90 @@ const stepWith = {
 };
 
 export const goalModel = {
-    getAll: () => {
+    // * Récupération de tous les objectifs d'un utilisateur
+    getAll: async (authorId: string, isAdmin: boolean = false) => {
         try {
-            return db.select({
-                id: goals.id,
-                title: goals.title,
-                description: goals.description,
-                userId: goals.userId,
-                dueDate: goals.dueDate,
-                status: goals.status,
-                createdAt: goals.createdAt,
-                updatedAt: goals.updatedAt,
-                startDate: goals.startDate,
-                difficulty: goals.difficulty,
-            }).from(goals);
+            const whereClause = isAdmin ? undefined : eq(goals.userId, authorId);
+
+            return await db.query.goals.findMany({
+                where: whereClause,
+            })
         } catch (err: any) {
-            logger.error(`Erreur lors de la récupération des objectifs : ${err.message}`);
+            // * Erreur détaillée pour le logger winston afin de faciliter le débogage et de ne rien remonter au client contenant des informations sensibles
+            logError('GOAL_MODEL_GETALL_ERROR', err, 'Erreur lors de la récupération des objectifs', {
+                id: null,
+                authorId,
+                isAdmin
+            })
             throw new Error('Erreur lors de la récupération des objectifs');
         }
     },
 
-    get: (id: string) => {
+    // * Récupération d'un objectif par son ID avec ses étapes, sous-étapes et tâches
+    get: async (id: string, authorId: string, isAdmin: boolean = false) => {
         try {
-            return db.query.goals.findFirst({
-                where: eq(goals.id, id),
-                columns: {
-                    id: true,
-                    title: true,
-                    description: true,
-                    userId: true,
-                    dueDate: true,
-                    status: true,
-                    createdAt: true,
-                    updatedAt: true,
-                    startDate: true,
-                    difficulty: true,
-                },
-                with: { steps: stepWith }
+            const whereClause = isAdmin ? eq(goals.id, id) : and(eq(goals.id, id), eq(goals.userId, authorId)); // Si l'utilisateur est admin, il peut récupérer n'importe quel objectif, sinon il ne peut récupérer que ses propres objectifs
+
+            return await db.query.goals.findFirst({
+                where: whereClause,
+                with: { steps: stepWith } // Joindre les étapes avec leurs sous-étapes et tâches, par exemple drizzle-orm va regarder dans le fichier de relations si goals à bien une relations "steps" pour ensuite trouver le nom correcte du schema associé
             });
         } catch (err: any) {
-            logger.error(`Erreur lors de la récupération de l'objectif : ${err.message}`);
+            logError('GOAL_MODEL_GET_ERROR]', err, "Erreur lors de la récupération de l'objectif", {
+                id,
+                authorId,
+                isAdmin
+            })
             throw new Error('Erreur lors de la récupération de l\'objectif');
         }
     },
 
-    create: (goal: NewGoal) => {
+    // * Création d'un nouvel objectif
+    create: async (goal: NewGoal, authorId: string) => { // goal équivaut à là base -> req.body
         try {
-            return db.insert(goals).values(goal).returning({ id: goals.id });
+            return await db.insert(goals).values({
+                ...goal,
+                userId: authorId,
+            }).returning(); // On retourne l'objectif créé pour afficher les données au client
         } catch (err: any) {
-            logger.error(`Erreur lors de la création de l'objectif : ${err.message}`);
+            logError('GOAL_MODEL_CREATE_ERROR', err, "Erreur lors de la création de l'objectif", {
+                id: null,
+                authorId,
+                isAdmin: null
+            })
             throw new Error('Erreur lors de la création de l\'objectif');
         }
     },
 
-    update: (id: string, data: Partial<Goal>) => {
+    // * Mise à jour d'un objectif
+    update: async (id: string, data: Partial<Goal>, authorId: string, isAdmin: boolean = false) => { // Partial pour permettre la mise à jour partielle en ts, isAdmin pour modifier en tant qu'admin
         try {
-            return db.update(goals).set(data).where(eq(goals.id, id));
+            const whereClause = isAdmin ? eq(goals.id, id) : and(eq(goals.id, id), eq(goals.userId, authorId)); // Si l'utilisateur est admin, il peut modifier n'importe quel objectif, sinon il ne peut modifier que ses propres objectifs
+
+            return await db.update(goals).set(data).where(whereClause);
         } catch (err: any) {
-            logger.error(`Erreur lors de la mise à jour de l'objectif : ${err.message}`);
+            logError('GOAL_MODEL_UPDATE_ERROR', err, "Erreur lors de la mise à jour de l'objectif", {
+                id,
+                authorId,
+                isAdmin,
+                data
+            })
             throw new Error('Erreur lors de la mise à jour de l\'objectif');
         }
     },
 
-    delete: (id: string, authorId: string) => {
+    // * Suppression d'un objectif
+    delete: async (id: string, authorId: string, isAdmin: boolean = false) => {
         try {
-            return db.delete(goals).where(
-                and(
-                    eq(goals.id, id),
-                    eq(goals.userId, authorId),
-                )
-            );
+            const whereClause = isAdmin ? eq(goals.id, id) : and(eq(goals.id, id), eq(goals.userId, authorId)); // Si l'utilisateur est admin, il peut supprimer n'importe quel objectif, sinon il ne peut supprimer que ses propres objectifs
+
+            return await db.delete(goals).where(whereClause);
         } catch (err: any) {
-            logger.error(`Erreur lors de la suppression de l'objectif : ${err.message}`);
+            logError('GOAL_MODEL_DELETE_ERROR', err, 'Erreur lors de la suppression de l\'objectif', {
+                id,
+                authorId,
+                isAdmin,
+            })
             throw new Error('Erreur lors de la suppression de l\'objectif');
         }
     }
