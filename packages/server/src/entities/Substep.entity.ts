a93676885357027a1f089544605238197aac65c2
evan <evan.azemard@acteam-it.com>
2025-06-06 03:47:45 +0200

    refactor(entities): change on Substep file new -> New

--- packages/server/src/entities/Substep.entity.ts
+++ packages/server/src/entities/Substep.entity.ts
@@ -3,4 +3,4 @@ import { InferInsertModel, InferSelectModel } from "drizzle-orm";
 import { substeps } from "../schemas";
 
 export type Substep = InferSelectModel<typeof substeps>;
-export type newSubstep = InferInsertModel<typeof substeps>;
+export type NewSubstep = InferInsertModel<typeof substeps>;
