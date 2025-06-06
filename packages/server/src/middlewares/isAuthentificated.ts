873c7de6a5e6cffabb5e4c03656c244e7cc19cba
evan <evan.azemard@acteam-it.com>
2025-06-06 11:48:43 +0200

    docs(isAuthentificated): update comments

--- packages/server/src/middlewares/isAuthentificated.ts
+++ packages/server/src/middlewares/isAuthentificated.ts
@@ -9,15 +9,15 @@ const { JWT_SECRET } = env;
 
 export const isAuthentificated = (request: Request, response: Response, next: NextFunction) => {
     
-    const { accessToken } = request.cookies;
+    const { accessToken } = request.cookies; // * Récupération de mon token enregistré dans un cookie que le navigateur renvoie à chaque requette dans le header
 
     if (!accessToken) {
-        logger.error("[MIDDLEWARES_IS-AUTHENTIFICATED] \n Authentification du jwt impossible");
+        logger.error("[MIDDLEWARES_IS-AUTHENTIFICATED] \n Authentification du jwt impossible, aucun JWT");
         return APIResponse(response, null, "Vous devez être connecté", 401);
     }
 
     try {
-        const decoded = jwt.verify(accessToken, JWT_SECRET);
+        const decoded = jwt.verify(accessToken, JWT_SECRET); // *  jwt Place les éléments décodé dans un objet local.user
         response.locals.user = decoded;
         next();
     } catch (err: any) {
